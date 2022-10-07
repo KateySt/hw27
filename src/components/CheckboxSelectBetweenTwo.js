802bacb5eab1973313544a642d627e2cbe816ed9
KateySt <kateystetsenko@gmail.com>
2022-10-07 12:44:07 +0300

    hw27

--- src/components/CheckboxSelectBetweenTwo.js
+++ src/components/CheckboxSelectBetweenTwo.js
@@ -4,21 +4,25 @@ import React from "react";
 const CheckboxSelectBetweenTwo = ({field}) => {
     const formikProps = useFormikContext();
 
+    const checkingFirstCheckbox = () => {
+        if (document.getElementById('checkbox1').checked === true) {
+            document.getElementById('checkbox2').checked = false;
+        }
+    }
+
+    const checkingSecondCheckbox =() => {
+        if (document.getElementById('checkbox2').checked === true) {
+            document.getElementById('checkbox1').checked = false;
+            formikProps.setFieldValue("wantGetUpdatesByEmail", false);
+        }
+    }
+
     return (
         <label className="text-gray-500 font-bold">
-            <input id="checkbox1" {...field}  type="checkbox" onClick={() => {
-                if (document.getElementById('checkbox1').checked === true) {
-                    document.getElementById('checkbox2').checked = false;
-                }
-            }}/>
+            <input id="checkbox1" {...field}  type="checkbox" onClick={checkingFirstCheckbox}/>
             <span>Confirm sending your email</span>
             <br/>
-            <input id="checkbox2" type="checkbox" onClick={() => {
-                if (document.getElementById('checkbox2').checked === true) {
-                    document.getElementById('checkbox1').checked = false;
-                    formikProps.setFieldValue("wantGetUpdatesByEmail", false);
-                }
-            }}/>
+            <input id="checkbox2" type="checkbox" onClick={checkingSecondCheckbox}/>
             <span>Do not send letters</span>
             <br/>
         </label>
